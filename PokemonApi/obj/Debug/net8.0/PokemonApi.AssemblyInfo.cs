ed3d651a29992106fe3b09b097439a4294081088
Ashenafidejene <ashenafidejene75@gmail.com>
2025-01-10 16:05:31 +0300

    comite

--- PokemonApi/obj/Debug/net8.0/PokemonApi.AssemblyInfo.cs
+++ PokemonApi/obj/Debug/net8.0/PokemonApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a77ae971c4c69560bbd5107b32039cf4a673979")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd73626b6a0487b83f29210a84f8c92afc0e057f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
